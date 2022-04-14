800a153100d320e64118936968fea36f6f0afa27
min1018 <102647148+min1018@users.noreply.github.com>
2022-04-14 15:13:42 +0900

    Update word_freq.py

--- word_freq.py
+++ word_freq.py
@@ -6,26 +6,25 @@ fname = sys.argv[1]
 num = int(sys.argv[2])
 
 if len(sys.argv) != 3:
-    print("명령인자 부족")
-    sys.exit()
+	print("명령인자 부족")
+	sys.exit()
 
 file = open(fname, 'r')
-
 word_cnt = dict()
 while True:
-    l = file.readline()
-    if not l: break
-    line = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·\n\t]', ' ', l) 
-  
-    for word in line.split():   
-      if word in word_cnt:
-        word_cnt[word] += 1
-      else:
-        word_cnt[word] = 1
-  
-sorted_dic = sorted(word_cnt.items(),key = lambda x: x[1],reverse = True)
+	l = file.readline()
+	if not l: break
 
-for r in range(0,num):
-   print("{:<10}{:>10}".format(sorted_dic[r][0],sorted_dic[r][1]))
+	line = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·\n\t]', ' ', l)
+	for word in line.split():
+		if word in word_cnt:
+			word_cnt[word] += 1
+		else:
+			word_cnt[word] = 1
+
+# 정렬
+sorted_dic = sorted(word_cnt.items(), key=lambda x: x[0], reverse=True)
+for r in range(num):
+	print("{:<10}{:>10}".format(word_cnt[r][0], word_cnt[r][1]))
 
 file.close()
