9009d79b1aaaaf3e6ae011f6b12a3c9ea0064754
min1018 <102647148+min1018@users.noreply.github.com>
2022-04-14 14:58:17 +0900

    Update word_freq.py

--- word_freq.py
+++ word_freq.py
@@ -15,7 +15,7 @@ while True:
 	l = file.readline()
 	if not l: break
 	
-	line = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·\n\t]', ' ', l)
+	line = re.sub('[^a-zA-z가-힣0-9]', ' ', l)
 	for word in line.split():
 		if word in word_cnt:
 			word_cnt[word] += 1
@@ -23,7 +23,7 @@ while True:
 			word_cnt[word] = 1
 
 #정렬
-
+sorted_dic = sorted(word_cnt.items(), key=lambda x: x[1], reverse=True)
 for r in range(0, num):
 	print("{:<10}{:>10}".format(word_cnt[r][0], word_cnt[r][1]))
 
