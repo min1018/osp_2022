334bd95626eecadeb81c5d4205bc5ae932109404
min1018 <102647148+min1018@users.noreply.github.com>
2022-04-14 15:09:03 +0900

    Update word_freq.py

--- word_freq.py
+++ word_freq.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# !/usr/bin/python3
 import sys
 import re
 
@@ -14,18 +15,17 @@ word_cnt = dict()
 while True:
 	l = file.readline()
 	if not l: break
-	
-	line = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·\n\t]', ' ', l) 
+
+	line = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·\n\t]', ' ', l)
 	for word in line.split():
 		if word in word_cnt:
 			word_cnt[word] += 1
 		else:
 			word_cnt[word] = 1
 
-#정렬
+# 정렬
 sorted_dic = sorted(word_cnt.items(), key=lambda x: x[0], reverse=True)
 for r in range(0, num):
 	print("{:<10}{:>10}".format(word_cnt[r][0], word_cnt[r][1]))
 
-	
 file.close()
